957b469fb8e5f62dca7df39ae92aee1372ff5336
Walter Cassarà <c.walter98@hotmail.it>
2024-03-20 20:09:51 +0100

    Fix active e cose varie

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+setActive = (li) => {
+  const active = document.querySelector('.active');
+  if (!!active) {
+    active.className = 'd-flex align-items-center nav-link';
+  }
+
+  li.className = 'd-flex align-items-center nav-link active';
+};
+
+const id = document.URL.split("#").pop() ?? "home";
+const element = document.querySelector("a#a-" + id);
+
+if (!!element) {
+  setActive(element);
+  window.location.hash = "";
+  window.location.hash = id;
+}
+
 const liActive = document.querySelectorAll('.nav-link');
 
-liActive.forEach((li) => {
-  li.addEventListener('click', () => {
-    const active = document.querySelector('.active');
-    active.className = 'nav-link';
-    li.className = 'nav-link active';
-  });
+liActive.forEach(li => {
+  li.addEventListener('click', () => setActive(li));
 });
