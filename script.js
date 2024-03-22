ed1dfb0195122cfe369dcdb5a7a92db4abf1dd91
Walter Cassarà <c.walter98@hotmail.it>
2024-03-22 01:59:16 +0100

    Aggiunto primo testo su home e relativa animazione; fix vari

--- script.js
+++ script.js
@@ -1,32 +1,69 @@
 /**
- * Funzione che aggiunge la classe "active" all'elemento corretto.
- * @param li Elemento li
+ * Animazione per scrivere il testo della sezione Home.
  */
-function setActive(li) {
-  const active = document.querySelector(".active");
+function writeTypedText() {
+  const title = "Nome Cognome";
+  const subtitle = "Il tuo sviluppatore web di fiducia";
+  let h1 = document.getElementById("title");
+  let h2 = document.getElementById("subtitle");
 
-  if (!!active) active.classList.remove("active");
+  // Animazione
+  typedText = (element, text, i = 0) => {
+    element.textContent += text[i];
+    if (i === text.length - 1) {
+      if (element === h1) {
+        document.querySelector("span.text-cursor-h1").classList.add("displayNone");
+        document.querySelector("span.text-cursor-h2").classList.remove("displayNone");
+        typedText(h2, subtitle);
+      }
+      return;
+    }
+    setTimeout(() => typedText(element, text, ++i), 50);
+  }
 
-  li.classList.add("active");
-};
+  typedText(h1, title);
+}
 
-document.querySelector("p.currentYear").innerHTML += ` ${new Date().getFullYear()}`;
 
+
+// Quando carica tutto aggiunge le classi per nascondere il loader e inizia a scrivere il testo della sezione Home
 window.onload = () => {
   let loader = document.getElementById("loader");
   loader.classList.add("hide");
-  setTimeout(() => loader.classList.add("displayNone"), 1000);
+  setTimeout(() => {
+    loader.classList.add("displayNone");
+
+    if (window.location.hash === "#home") writeTypedText();
+  }, 1000);
 };
 
-const id = document.URL.split("#").pop() ?? "home";
-const element = document.querySelector("a#a-" + id);
 
-if (!!element) {
-  setActive(element);
-  window.location.hash = "";
-  window.location.hash = id;
-}
 
-const liActive = document.querySelectorAll(".nav-link");
+// Aggiunge l'anno corrente all'elemento copyright
+document.querySelector("p.currentYear").innerHTML += ` ${new Date().getFullYear()}`;
 
-liActive.forEach(li => li.addEventListener("click", () => setActive(li)));
+
+
+// Imposta la classe "active" all'elemento corretto e fa partire la scrittura del testo della sezione Home se ci si sposta su di essa
+const tmpHash = window.location.hash;
+window.location.hash = "";
+window.location.hash = tmpHash === "" ? "home" : tmpHash;
+let isWritten = window.location.hash === "#home";
+
+addEventListener("hashchange", (event) => {
+  
+  const hash = event.currentTarget.location.hash;
+  const id = hash.split("").slice(1).join("");
+  const element = document.querySelector("a#a-" + id);
+  
+  if (!!element) {
+    const active = document.querySelector(".active");
+    if (!!active) active.classList.remove("active");
+    element.classList.add("active");
+  }
+
+  if (hash === "#home" && !isWritten) {
+    writeTypedText();
+    isWritten = true;
+  }
+});
